83e9023adc8253f6610e3b3a0227015011a668ef
Graeme Harrison <gharrison365@gmail.com>
2016-01-08 12:15:19 -0500

    Completed Algorhythm app. First commit to github repo.

---
+++ Algorhythm/PlaylistDetailViewController.h
@@ -0,0 +1,23 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorhythm
+//
+//  Created by Graeme Harrison on 2015-12-29.
+//  Copyright © 2015 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Playlist;
+
+@interface PlaylistDetailViewController : UIViewController
+
+@property (strong, nonatomic) Playlist *playlist;
+@property (weak, nonatomic) IBOutlet UIImageView *playlistCoverImage;
+@property (weak, nonatomic) IBOutlet UILabel *playlistTitle;
+@property (weak, nonatomic) IBOutlet UILabel *playlistDescription;
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist0;
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist1;
+@property (weak, nonatomic) IBOutlet UILabel *playlistArtist2;
+
+@end
